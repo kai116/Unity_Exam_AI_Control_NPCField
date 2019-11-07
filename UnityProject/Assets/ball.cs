58550248b3567452ddf490e6d491694b1a558189
kai116 <55518563+kai116@users.noreply.github.com>
2019-11-07 10:54:16 +0800

    期中

---
+++ UnityProject/Assets/ball.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class ball : MonoBehaviour {
+
+    [Range(20, 1000)]
+    public float speed = 30;
+    [Range(20, 1000)]
+    public int jump = 30;
+    Rigidbody RRR;
+    public bool groundornot;
+
+
+    private void Start()
+    {
+
+        RRR = this.GetComponent<Rigidbody>();
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        groundornot = true;
+    }
+
+    private void Update()
+    {
+        walk();
+        jujump();
+    }
+
+    private void walk()
+    {
+
+        RRR.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, 0));
+        RRR.AddForce(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
+
+    }
+
+    private void jujump()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundornot == true)
+        {
+            groundornot = false;
+            RRR.AddForce(new Vector3(0, jump, 0));
+        }
+
+    }
+
+}
---
+++ UnityProject/Assets/robot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class robot : MonoBehaviour {
+
+    [Header("TALK")]
+    public string  SAY;
+    public float sayspeed = 1.3f;
+
+    [Header("mission")]
+    public bool complete;
+    public float bateryfinish = 1.1f;
+    public float batery = 0f;
+
+}
